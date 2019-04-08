13fea0d3f674856768537142a253ba2a8b1cda50
David Donovan <daviddonovan5@gmail.com>
2019-04-08 16:20:53 -0700

    adding database

--- server.js
+++ server.js
@@ -130,7 +130,7 @@ function deleteRows(rows){
 
       rows.forEach(function(element) {
         console.log(element);
-        const result = await client.query("DELETE FROM wishlist WHERE bookid =" + element + ";");
+        //const result = await client.query("DELETE FROM wishlist WHERE bookid =" + element + ";");
    });
       res.render('pages/delete', results );
       client.release();
